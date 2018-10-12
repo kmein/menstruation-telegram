e9d193e6e685c33d9a3bc029837454e1d027d194
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2018-10-12 20:18:43 +0200

    Pimp shell.nix

--- shell.nix
+++ shell.nix
@@ -1,15 +1,14 @@
 { pkgs ? import <nixpkgs> {} }:
-let python = import ./requirements.nix { inherit pkgs; };
-in pkgs.stdenv.mkDerivation {
+pkgs.stdenv.mkDerivation {
   name = "menstruation";
   buildInputs = with pkgs; [
-    automake autoconf libtool # for jq
     libxml2 libxslt # for gdom
+    automake autoconf libtool # for jq
   ];
   shellHook = ''
     set -f
-    test -e venv || python -m venv venv
+    test -e venv || python3 -m venv venv
     source venv/bin/activate
-    pip install gdom jq termcolor
+    pip3 install -r requirements.txt
   '';
 }
