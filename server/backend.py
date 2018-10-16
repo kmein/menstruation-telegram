702ada164329b8a91309aba077bd80c9411b4391
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2018-10-17 00:21:14 +0200

    Major update: It works now!!!
    ~ split into client and server
    ~ server: use beautifulsoup and useful endpoint, use flask
    + server: list mensa codes
    + client: date option
    + client: mensa code option
    + client: list mensa codes
    ~ shell.nix: remove gdom and jq, add $FLASK_APP

---
+++ server/backend.py
@@ -0,0 +1,97 @@
+from bs4 import BeautifulSoup
+from datetime import date, datetime
+from enum import Enum
+import json
+import re
+import requests
+
+DEFAULT_HEADERS = {"User-Agent": "Mozilla/5.0"}
+
+def meals_html_for_day(mensa_code, date):
+    return requests.post(
+        "https://www.stw.berlin/xhr/speiseplan-wochentag.html",
+        headers=DEFAULT_HEADERS,
+        data={
+            "week": "now",
+            "date": date.isoformat(),
+            "resources_id": mensa_code,
+        }).text
+
+def icon_to_tag(icon):
+    return {
+        "/vendor/infomax/mensen/icons/1.png": "vegetarian",
+        "/vendor/infomax/mensen/icons/15.png": "vegan",
+        "/vendor/infomax/mensen/icons/18.png": "organic",
+        "/vendor/infomax/mensen/icons/38.png": "sustainable fishing",
+        "/vendor/infomax/mensen/icons/43.png": "climate",
+        "/vendor/infomax/mensen/icons/ampel_gelb_70x65.png": "yellow",
+        "/vendor/infomax/mensen/icons/ampel_gruen_70x65.png": "green",
+        "/vendor/infomax/mensen/icons/ampel_rot_70x65.png": "red",
+    }[icon]
+
+def extract_meals(html):
+    soup = BeautifulSoup(html, "html.parser")
+    groups = []
+    for group in soup.find_all(class_="splGroupWrapper"):
+        group_name = group.find(class_="splGroup").text.strip()
+        meals = []
+        for meal in group.find_all(class_="splMeal"):
+            icons = [img["src"] for img in meal.find_all("img", class_="splIcon")]
+            meal_name = meal.find("span", class_="bold").text.strip()
+            student, employee, guest = (
+                float(price)
+                for price
+                in meal.find("div", class_="text-right").text.replace("€ ", "").replace(",", ".").split("/")
+            )
+            allergen_matches = re.search("\((.*)\)", meal.find(class_="toolt").text)
+            if allergen_matches:
+                allergens = allergen_matches.group(1).split(", ")
+            else:
+                allergens = []
+            meals.append({
+                "name": meal_name,
+                "tags": [icon_to_tag(icon) for icon in icons],
+                "price": {
+                    "student": student,
+                    "employee": employee,
+                    "guest": guest,
+                },
+                "allergens": allergens,
+            })
+        groups.append({
+            "name": group_name,
+            "meals": meals,
+        })
+    return {"groups": groups}
+
+def meals(mensa_code, date):
+    return extract_meals(meals_html_for_day(mensa_code, date))
+
+###
+
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+app.config["JSON_AS_ASCII"] = False
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
+
+@app.route("/codes")
+def list_codes():
+    html = requests.get("https://www.stw.berlin/mensen.html", headers=DEFAULT_HEADERS).text
+    soup = BeautifulSoup(html, "html.parser")
+    unis = []
+    for uni in soup.find(id="itemsHochschulen").find_all(class_="container-fluid"):
+        name = uni.find("h4").text.strip()
+        mensas = []
+        for mensa in uni.find_all(class_=["row", "row-top-percent-1", "ptr"], onclick=True):
+            code = mensa["onclick"][9:12]
+            address = re.sub("\s+", " ", mensa.find(class_="addrcard").text.strip().replace("\n", " /"))
+            mensas.append({"code": code, "address": address})
+        unis.append({"name": name, "mensas": mensas})
+    return jsonify({"unis": unis})
+
+@app.route("/<mensa_code>", defaults={"isodate": date.today().isoformat()})
+@app.route("/<mensa_code>/<isodate>")
+def get_date(mensa_code, isodate):
+    date = datetime.strptime(isodate, "%Y-%m-%d")
+    return jsonify(meals(mensa_code, date))
--- shell.nix
+++ shell.nix
@@ -6,15 +6,14 @@ stdenv.mkDerivation {
     python36Packages.pip
     python36Full
     python36Packages.virtualenv
-
-    libxml2 libxslt # for gdom
-    automake autoconf libtool # for jq
   ];
 
   shellHook = ''
     SOURCE_DATE_EPOCH=$(date +%s)
     virtualenv --no-setuptools venv
     export PATH=$PWD/venv/bin:$PATH
-    pip install -r requirements.txt
+    pip install -r server/requirements.txt -r client/requirements.txt
   '';
+
+  FLASK_APP = "server/backend.py";
 }
